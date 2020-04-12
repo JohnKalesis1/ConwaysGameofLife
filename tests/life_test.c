140e3619f6aa009f3ec5840bcafafb1e0cb28820
JohnKalesis1 <sdi1900069@di.uoa.gr>
2020-04-12 23:42:45 +0300

    done

--- tests/life_test.c
+++ tests/life_test.c
@@ -2,8 +2,11 @@
 #include <stdlib.h>
 #include "life.h"
 #include <string.h>
+int compare_states(Pointer a,Pointer b);
 int main(void)  {
     LifeState Universe;
+    List list;
+    ListNode node;
     char text[500];
     char *light_spaceship="../programs/Game_Life_Gif/lightweight_spaceship.txt";
     char *glider="../programs/Game_Life_Gif/glider.txt";
@@ -25,12 +28,34 @@ int main(void)  {
     Universe=life_create_from_rle(period_5_oscillator);
     life_save_to_rle(Universe,file);
     fp=fopen(file,"r");
-    fscanf(fp,"%s",text);
+    fscanf(fp,"%s",text); //βασικος ελεγχος για σωστη η αντιγραφη στοιχειων απο κειμενο και σε κειμενο 
     if (strcmp(text,"4b2o6b2o$3bobo6bobo$3bo10bo$2obo10bob2o$2obobo2b2o2bobob2o$3bobobo2bobobo$3bobobo2bobobo$2obobo2b2o2bobob2o$2obo10bob2o$3bo10bo$3bobo6bobo$4b2o6b2o!")!=0)  {
         printf("Error in function\n");
     }
     else  {
         printf("Passed \n");
+        list=life_evolve_many(Universe,6,&node);
+        ListNode lnode=list_first(list);
+        int i=4;
+        while (i--!=0)  {
+            lnode=list_next(list,lnode);
+        }
+        LifeState U=list_node_value(list,lnode); //δουλευει και η συναρτηση πολλαπλης εξελιξης 
+        if (compare_states(Universe,U)==0)  {   //καθως μετα απο πεντε εξελιξεις επιστρεφουμε στην αρχικη μορφη
+            printf("Passed \n");
+            lnode=list_first(list);
+            LifeState U=list_node_value(list,lnode);
+            LifeState Y =list_node_value(list,node);
+            if (compare_states(Y,U)==0)  {
+                printf("Passed \n"); //δουλευει η καταχωρηση της επαναληψης σε κομβο 
+            }                        //δηλαδη το πρωτο στοιχειο που παρατηρηθηκε η επαναληψη , αποθηεκευεται σε εναν κομβο
+            else  {
+            printf("Error \n");
+        }
+        }
+        else  {
+            printf("Error \n");
+        }
     }
     fclose(fp);
     Universe=life_create_from_rle(period_2_oscillator);
@@ -42,6 +67,16 @@ int main(void)  {
     }
     else  {
         printf("Passed \n");
+        list=life_evolve_many(Universe,2,&node);
+        ListNode lnode=list_first(list);
+        lnode=list_next(list,lnode);
+        LifeState U=list_node_value(list,lnode);
+        if (compare_states(Universe,U)==0)  {
+            printf("Passed \n");
+        }
+        else  {
+            printf("Error \n");
+        }
     }
     fclose(fp);
     Universe=life_create_from_rle(glider);
@@ -53,6 +88,8 @@ int main(void)  {
     }
     else  {
         printf("Passed \n");
+        
     }
     fclose(fp);
+
 }
